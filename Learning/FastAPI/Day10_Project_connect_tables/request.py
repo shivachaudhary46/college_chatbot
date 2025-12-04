9d213ffea4af042123225891b816f8802289c016
shivachaudhary46 <shivachaudhary4116@gmail.com>
2025-12-04 11:54:36 +0545

    removing learning folder and restructuring a bit

--- Learning/FastAPI/Day10_Project_connect_tables/request.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:8000/users/me/"
-headers = {"Authorization": "Bearer test123"}
-
-response = requests.get(url, headers=headers)
-user = response.json()
-print(user)
--- Learning/project/variable.py
+++
@@ -1,5 +0,0 @@
-import os 
-
-hf_key = os.environ["HUGGINGFACEHUB_API_TOKEN"]
-api_key = os.environ["GOOGLE_API_KEY"]
-INDEX_NAME = "mbmc-data"
